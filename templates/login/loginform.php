6b8dcf78bacdc7508ff5f75c2454931fb67bd48e
Dzmitry2020 <pupkinvas378@gmail.com>
2020-08-09 11:34:09 +0300

    Update loginform.php

--- templates/login/loginform.php
+++ templates/login/loginform.php
@@ -20,7 +20,7 @@ $form = Html::Form()
 $form->addInnerText(Html::Input()
     ->setName("login")
     ->setId("login")
-    ->setPlaceholder("🧑 Логин")
+    ->setPlaceholder("Введите логин")
     ->html());
 
 //$form->addInnerText(Html::Label()
@@ -32,7 +32,7 @@ $form->addInnerText(Html::Input()
     ->setType('password')
     ->setName("pass")
     ->setId("pass")
-    ->setPlaceholder("🔑 Пароль")
+    ->setPlaceholder("Введите пароль")
     ->html());
 
 $form->addInnerText(
